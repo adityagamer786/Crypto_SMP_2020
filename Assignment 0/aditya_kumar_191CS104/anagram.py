a920c6887a237754d32b4cf36e61531319456bc7
adityagamer786 <adityagamers786@gmail.com>
2020-04-12 19:57:04 +0530

    Changed anagram to a better algorithml

--- Assignment 0/aditya_kumar_191CS104/anagram.py
+++ Assignment 0/aditya_kumar_191CS104/anagram.py
@@ -1,30 +1,35 @@
 from random import randint
+from math import ceil, sqrt
 
 s1 = input("Enter string 1:")
 s2 = input("Enter string 2:")
 
-if len(s1) != len(s2):
-    print('Not anagram')
+len1, len2 = 0, 0
+
+for ch in s1: len1 += 1
+for ch in s2: len2 += 1
+
+if len1 != len2:
+    print("Not anagram")
     exit(0)
 
-ag = []
-bg = []
-for i in range(5):
-    ag.append(randint(1,25))
-for i in range(5):
-    bg.append(randint(0,25))
+primes = []
+primes.append(2)
+primes_count, i = 1, 3
+while(primes_count < 26):
+    it_is_prime=1
+    for j in range(2, ceil(sqrt(i))+1):
+        if i % j == 0:
+            it_is_prime=0
+            break
+    if it_is_prime:
+        primes.append(i)
+        primes_count += 1
+    i += 2
 
-for a in ag:
-    for b in bg:
-        hash1 = 0
-        for ch in s1:
-            hash1 += (a*ord(ch) + b) % 29
-        hash2 = 0
-        for ch in s2:
-            hash2 += (a*ord(ch) + b) % 29
-        if hash1 != hash2:
-            print("Not anagram")
-            exit(0)
+prod1, prod2 = 1, 1
 
-print("The probability that they are not anagram is same as that of a large UFO hitting the earth in next 1 second")
-print("In other words, they are anagram")
+for ch in s1: prod1 *= primes[ord(ch)-97]
+for ch in s2: prod2 *= primes[ord(ch)-97]
+
+print("Anagram") if prod1 == prod2 else print("Not Anagram")
