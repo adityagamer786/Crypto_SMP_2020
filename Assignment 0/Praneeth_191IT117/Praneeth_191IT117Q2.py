92c8d8e3ba8db8e5d6d933839f5cef921cfe1514
GosuPraneeth <praneethgosu322@gmail.com>
2020-04-14 19:38:18 +0530

    Assignment_0

--- Assignment 0/Praneeth_191IT117/Praneeth_191IT117Q2.py
+++ Assignment 0/Praneeth_191IT117/Praneeth_191IT117Q2.py
@@ -1,8 +1,10 @@
 x=int(input("Enter the number\n"))
 i=1
+n=0
 while i*i<=x:
         if x%i==0:
-                print(i,end=" ")
+                n=n+1
                 if x//i!=i:
-                        print(x//i,end=" ")
+                        n=n+1
         i=i+1
+print(n)
