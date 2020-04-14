b888dcf8cc0fc2a2e66ed41392666f811cb4e52d
Karn Tiwari <53399509+Karn-Tiwari-3003@users.noreply.github.com>
2020-04-14 21:20:42 +0530

    Delete IE_crypt_02.py
    multiple files

--- Assignment_0/IE_crypt_02.py
+++
@@ -1,8 +0,0 @@
-N, count = int(input("Enter a number : ")), 0
-for i in range(1, int(N**0.5) + 1):
-    if N%i == 0:
-        if N == i*i:
-            count += 1
-        else:
-            count += 2
-print(count)
