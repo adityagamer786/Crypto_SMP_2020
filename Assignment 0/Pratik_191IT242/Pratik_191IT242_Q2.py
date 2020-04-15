a2001aabe28df91c1acb998ea8348ce74b940a85
Pratik <pratikshivnani@gmail.com>
2020-04-15 23:18:57 +0530

    Assignment 0

---
+++ Assignment 0/Pratik_191IT242/Pratik_191IT242_Q2.py
@@ -0,0 +1,11 @@
+n = int(input('Enter the Number'))
+N_f = 2
+f = 2
+while f*f <= n:
+    if(n%f == 0):
+        if(n % (n/f) == 0) and f*f != n:
+            N_f += 2
+        else:
+            N_f += 1
+    f+=1
+print(N_f)
